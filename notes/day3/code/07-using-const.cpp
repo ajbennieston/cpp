f7d13b85d6bd3c0ace9b94ea496e8aa0252e7015
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2013-03-04 12:39:17 +0000

    Add two more source files for day 3 (issue #20)

---
+++ notes/day3/code/07-using-const.cpp
@@ -0,0 +1,23 @@
+/* 
+ * C++ Notes Accompanying Code
+ * Compile: Y
+ * Run: Y
+ * Compile Should Succeed: Y
+ * Run Should Succeed: Y
+ */
+
+// NOTES: BEGIN INCLUSION
+double interval(const double t, const double x,
+                const double y, const double z);
+
+// underscore suffix denotes primed variables
+double boost_z(const double t, const double x, const double y,
+               const double z, double& t_, double& x_,
+               double& y_, double& z_, const double velocity);
+// NOTES: END INCLUSION
+
+int main()
+{
+    return 0;
+}
+
---
+++ notes/day3/code/08-placement-of-const.cpp
@@ -0,0 +1,17 @@
+/* 
+ * C++ Notes Accompanying Code
+ * Compile: Y
+ * Run: Y
+ * Compile Should Succeed: Y
+ * Run Should Succeed: Y
+ * Ignore Unused Variables: Y
+ */
+
+int main()
+{
+    // NOTES: BEGIN INCLUSION
+    const double t1(3.14);
+    double const t2(3.14);
+    // NOTES: END INCLUSION
+    return 0;
+}
