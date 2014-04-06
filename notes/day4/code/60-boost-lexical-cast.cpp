0f7f6ecdc8ff1ca449c735772d57272b3bdf8494
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2014-04-06 18:57:04 +0100

    Finish splitting day4 code from text. Issue #20.

---
+++ notes/day4/code/60-boost-lexical-cast.cpp
@@ -0,0 +1,13 @@
+/* 
+ * C++ Notes Accompanying Code
+ * Compile: N
+ * Run: N
+ * Compile Should Succeed: N
+ * Run Should Succeed: N
+ */
+
+// NOTES: BEGIN INCLUSION
+std::string str = "0.707";
+double x = boost::lexical_cast<double>(str);
+// NOTES: END INCLUSION
+
